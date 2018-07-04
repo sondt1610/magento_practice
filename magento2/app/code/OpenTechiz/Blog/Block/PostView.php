9a5eb6e03399cecb1d6d3dfab44cbf80ab2c7756
sondt1610 <sondt1610@gmail.com>
2018-07-04 17:49:58 +0700

    rewrite url key

---
+++ magento2/app/code/OpenTechiz/Blog/Block/PostView.php
@@ -0,0 +1,49 @@
+<?php
+namespace OpenTechiz\Blog\Block;
+
+class PostView extends \Magento\Framework\View\Element\Template implements
+    \Magento\Framework\DataObject\IdentityInterface
+{
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \OpenTechiz\Blog\Model\Post $post,
+        \OpenTechiz\Blog\Model\PostFactory $postFactory,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+        $this->_post = $post;
+        $this->_postFactory = $postFactory;
+    }
+
+
+    public function getPost()
+    {
+        // Check if posts has already been defined
+        // makes our block nice and re-usable! We could
+        // pass the 'posts' data to this block, with a collection
+        // that has been filtered differently!
+        if (!$this->hasData('post')) {
+            if ($this->getPostId()) {
+                /** @var \OpenTechiz\Blog\Model\Post $page */
+                $post = $this->_postFactory->create();
+            } else {
+                $post = $this->_post;
+            }
+            $this->setData('post', $post);
+        }
+        return $this->getData('post');
+    }
+
+    /**
+     * Return identifiers for produced content
+     *
+     * @return array
+     */
+    public function getIdentities()
+    {
+        return [\OpenTechiz\Blog\Model\Post::CACHE_TAG . '_' . $this->getPost()->getId()];
+    }
+
+}
