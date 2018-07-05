a594f70428a616fd36d7fd55bd860de37f8c1a70
sondt1610 <sondt1610@gmail.com>
2018-07-05 10:59:55 +0700

    update sql part 4

--- sql_part4/part_4.sql
+++ sql_part4/part_4.sql
@@ -1,5 +1,7 @@
 1. Query  all  product  name  for  store  English  level  
 
+SELECT IF(B.value IS NOT NULL, B.value, A.value) as name FROM (SELECT * FROM catalog_product_entity_varchar WHERE attribute_id = 73 AND store_id = 0) AS A LEFT JOIN (SELECT * FROM catalog_product_entity_varchar WHERE attribute_id = 73 AND store_id = 1) AS B ON A.entity_id = B.entity_id
+
 SELECT entity_id, value FROM catalog_product_entity_varchar
   WHERE store_id = (
     SELECT store_id FROM store
@@ -17,9 +19,9 @@ SELECT entity_id, value FROM catalog_product_entity_varchar
 2. Query  all  customer  name,  customer  address
 
 select 
-    email, CONCAT(a.firstname, " ", a.lastname, "\n") AS "Full name", 
+    email, CONCAT(c.firstname, " ", c.lastname, "\n") AS "Full name", 
     CONCAT(a.street, " ", a.city, "\n") AS "Full address"
 from
     customer_entity as c
-        inner join
+        left join
     customer_address_entity as a ON a.parent_id = c.entity_id
