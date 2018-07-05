815364fbc959a853f960f2749cf6f4afa3b83b8d
sondt1610 <sondt1610@gmail.com>
2018-07-05 09:25:44 +0700

    Show posts list in backend with ui component

---
+++ magento2/app/code/OpenTechiz/Blog/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace OpenTechiz\Blog\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+class Index extends Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Magento_Backend::content');
+        $resultPage->getConfig()->getTitle()->prepend(__('Blog Manager'));
+
+        return $resultPage;
+    }
+}
